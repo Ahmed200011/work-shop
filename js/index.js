66a033d5d5ae079497a6396bad80db9dc03c9dac
Ahmed200011 <ahmed.w632@yahoo.com>
2023-10-02 21:30:14 +0300

    modified:   css/style.css 	modified:   index.html 	modified:   js/index.js 	new file:   photo/WhatsApp Image 2023-09-27 at 00.41.54_d4fd5b6a.jpg 	deleted:    photo/imagegif.gif 	new file:   photo/sec-imgs/product-1.jpg 	new file:   photo/sec-imgs/product-10.jpg 	new file:   photo/sec-imgs/product-11.jpg 	new file:   photo/sec-imgs/product-12.jpg 	new file:   photo/sec-imgs/product-2.jpg 	new file:   photo/sec-imgs/product-3.jpg 	new file:   photo/sec-imgs/product-4.jpg 	new file:   photo/sec-imgs/product-5.jpg 	new file:   photo/sec-imgs/product-6.jpg 	new file:   photo/sec-imgs/product-7.jpg 	new file:   photo/sec-imgs/product-8.jpg 	new file:   photo/sec-imgs/product-9.jpg

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 var coverflow = $("#coverflow").flipster();
+
+var countDownDate = new Date("oct 9, 2023").getTime();
+console.log(countDownDate);
+
+
+var x = setInterval(function () {
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    
+    document.getElementById("day").innerHTML= days ;
+    document.getElementById("hour").innerHTML=  hours;
+    document.getElementById("minuts").innerHTML= minutes;
+    document.getElementById("seconds").innerHTML= seconds ;
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
