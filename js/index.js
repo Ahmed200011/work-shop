1ad5c6b96bbb3cdfa78b0422ce589eb0ef8ef967
Ahmed200011 <ahmed.w632@yahoo.com>
2023-10-16 19:04:50 +0300

    modified:   css/style.css 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -47,6 +47,6 @@ var x = setInterval(function () {
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        // document.getElementById("demo").innerHTML = "EXPIRED";
     }
 }, 1000);
