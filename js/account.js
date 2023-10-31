4acc4f009eaa4b0148a502513d783257b29f3aaa
Ahmed200011 <ahmed.w632@yahoo.com>
2023-11-01 00:34:31 +0200

    new file:   acount.html 	new file:   css/acount.css 	modified:   index.html 	new file:   js/account.js 	modified:   login.html 	new file:   photo/ee.jpeg 	new file:   photo/sale50.jpg 	modified:   register.html

---
+++ js/account.js
@@ -0,0 +1,67 @@
+// var text = document.querySelector("a.text");
+var left = document.getElementById("left");
+let tbody = document.querySelector(".tbody");
+tbody.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn")) {
+    var id = e.target.getAttribute("id");
+    var input = document.getElementById(id);
+    input.removeAttribute("disabled");
+    input.focus();
+  }
+  // if (input.blur()) {
+  //   input.setAttribute("disabled", "false");
+  // }
+  /////////
+  // input.blur = function () {
+  //   input.setAttribute("disabled", "false");
+  // };
+});
+
+
+left.addEventListener("click", function (e) {
+  if (e.target.classList.contains("text")) {
+    e.target.classList.add("activ");
+  }
+
+});
+
+
+let btn1=document.getElementById("btn1");
+btn1.onclick=function(){
+  document.querySelector(".btn1").setAttribute("class","none") 
+ 
+}
+let btn2=document.getElementById("btn2");
+btn2.onclick=function(){
+  document.querySelector(".btn2").setAttribute("class","none") 
+ 
+}
+let btn3=document.getElementById("btn3");
+btn3.onclick=function(){
+  document.querySelector(".btn3").setAttribute("class","none") 
+ 
+}
+let btn4=document.getElementById("btn4");
+btn4.onclick=function(){
+  document.querySelector(".btn4").setAttribute("class","none") 
+}
+let btn5=document.getElementById("btn5");
+btn5.onclick=function(){
+  document.querySelector(".btn5").setAttribute("class","none") 
+}
+let btn6=document.getElementById("btn6");
+btn6.onclick=function(){
+  document.querySelector(".btn6").setAttribute("class","none") 
+}
+let btn7=document.getElementById("btn7");
+btn7.onclick=function(){
+  document.querySelector(".btn7").setAttribute("class","none") 
+}
+let btn8=document.getElementById("btn8");
+btn8.onclick=function(){
+  document.querySelector(".btn8").setAttribute("class","none") 
+}
+
+// function handelColor() {
+//   text.classList.add("activ");
+// }
